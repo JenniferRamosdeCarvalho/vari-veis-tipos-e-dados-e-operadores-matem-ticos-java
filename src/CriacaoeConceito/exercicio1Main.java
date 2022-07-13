4e73a93a742fd8fd8be7771ba726d380a6d3daf3
Perkles <jenniferramosdecarvalho@gmail.com>
2022-07-13 15:09:10 -0300

    regras e boas práticas

---
+++ src/CriacaoeConceito/exercicio1Main.java
@@ -0,0 +1,49 @@
+package CriacaoeConceito;
+
+public class exercicio1Main {
+    public static void main(String[] args) {
+
+        int i;
+        ///int i;
+        int I;
+        ///int 1a;
+        int _1a;
+        int $1a;
+
+        i = 5;
+        I = 10;
+        _1a = 20;
+        $1a = 7;
+
+        final int j = 10;
+        ///j = 15;
+        int arsn1877t;
+        ///int arns 768879;
+        int arns870$6_768;
+        ///int arns56$_869%868 = 10;
+
+        arsn1877t = 100;
+        arns870$6_768 = 1000;
+
+        int quantidadeProduto = 10;
+        ///int QuantidadeProduto;
+        final int NUMERO_TENTATIVAS = 5;
+        ///final int numeroTentativas = 5;
+        int QUANTIDADE_OPCOES = 5;
+        ///int qtdProd;
+
+        System.out.println(i);
+        System.out.println(I);
+        System.out.println(_1a);
+        System.out.println($1a);
+
+        System.out.println(j);
+        System.out.println(arsn1877t);
+        System.out.println(arns870$6_768);
+
+        System.out.println(quantidadeProduto);
+        System.out.println(NUMERO_TENTATIVAS);
+        System.out.println(QUANTIDADE_OPCOES);
+
+    }
+}
